8d5020aaa2a51749f4ae14df99cec5d8463b3540
Kazuhiro Oinuma <oinume@gmail.com>
2020-06-14 19:04:11 +0900

    Unmarsharl to response

--- backend/http_server/server_test.go
+++ backend/http_server/server_test.go
@@ -2,47 +2,78 @@ package http_server_test
 
 import (
 	"bytes"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/golang/protobuf/jsonpb"
-	"github.com/oinume/todomvc/backend/http_server"
 
 	"github.com/oinume/todomvc-example/proto-gen/go/proto/todomvc"
+	"github.com/oinume/todomvc/backend/http_server"
 )
 
 func Test_Server_CreateTodo(t *testing.T) {
 	m := &jsonpb.Marshaler{OrigName: true}
-	var reqBody bytes.Buffer
-	if err := m.Marshal(&reqBody, &todomvc.CreateTodoRequest{
-		Title: "New task",
-	}); err != nil {
-		t.Fatal(err)
-	}
-
-	req, err := http.NewRequest("POST", "/todos", &reqBody)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rr := httptest.NewRecorder()
-	defer func() { _ = rr.Result().Body.Close() }()
-
+	u := &jsonpb.Unmarshaler{}
 	s := http_server.New()
-	s.CreateTodo(rr, req)
-	result := rr.Result()
-	if result.StatusCode != 201 {
-		t.Errorf("unexpected status code: got=%v, want=%v", result.StatusCode, 201)
-	}
-	body, err := ioutil.ReadAll(result.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() { _ = result.Body.Close() }()
 
-	if len(body) == 0 {
-		t.Error("empty response reqBody")
+	type response struct {
+		statusCode int
+		todoItem   *todomvc.TodoItem
 	}
-	// TODO: Unmarshal to TodoItem
+	tests := map[string]struct {
+		request      *todomvc.CreateTodoRequest
+		wantResponse response
+	}{
+		"OK_Created": {
+			request: &todomvc.CreateTodoRequest{
+				Title: "New task",
+			},
+			wantResponse: response{
+				statusCode: http.StatusCreated,
+				todoItem: &todomvc.TodoItem{
+					Title:     "New task",
+					Completed: false,
+				},
+			},
+		},
+	}
+
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			var reqBody bytes.Buffer
+			if err := m.Marshal(&reqBody, test.request); err != nil {
+				t.Fatal(err)
+			}
+			req, err := http.NewRequest("POST", "/todos", &reqBody)
+			if err != nil {
+				t.Fatal(err)
+			}
+			rr := httptest.NewRecorder()
+			defer func() { _ = rr.Result().Body.Close() }()
+
+			s.CreateTodo(rr, req)
+
+			result := rr.Result()
+			if result.StatusCode != http.StatusCreated {
+				t.Errorf("unexpected status code: got=%v, want=%v", result.StatusCode, http.StatusCreated)
+			}
+			got := &todomvc.TodoItem{}
+			if err := u.Unmarshal(result.Body, got); err != nil {
+				t.Fatal(err)
+			}
+			if got.Id == "" {
+				t.Fatal("got.ID is empty")
+			}
+			if got, want := got.Title, test.request.Title; got != want {
+				// TODO: Use go-cmp
+				t.Fatalf("unexpected Title: got=%v, want=%v", got, want)
+			}
+		})
+	}
 }
+
+// TODO: Add error cases
