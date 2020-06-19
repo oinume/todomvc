0f49ebd455f3ec060e57adbebbe3427fc3dc61f6
Kazuhiro Oinuma <oinume@gmail.com>
2020-06-19 12:08:26 +0900

    Configure jaeger exporter

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -7,7 +7,9 @@ import (
 	"net/http"
 	"os"
 
+	"contrib.go.opencensus.io/exporter/jaeger"
 	_ "github.com/go-sql-driver/mysql"
+	"go.opencensus.io/trace"
 	"go.uber.org/zap"
 
 	"github.com/oinume/todomvc/backend/config"
@@ -23,18 +25,30 @@ func main() {
 
 	config.MustProcessDefault()
 
-	db, err := sql.Open("mysql", config.DefaultVars.DBURL())
+	// TODO: Stackdriver exporter
+	exporter, err := jaeger.NewExporter(jaeger.Options{
+		AgentEndpoint:     config.DefaultVars.JaegerAgentEndpoint,
+		CollectorEndpoint: config.DefaultVars.JaegerCollectorEndpoint,
+		Process: jaeger.Process{
+			ServiceName: "todomvc",
+		},
+	})
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+	trace.RegisterExporter(exporter)
+
+	db, err := sql.Open("mysql", config.DefaultVars.DBURL())
+	// TODO: db.SetMaxIdleConns, etc...
+	if err != nil {
+		logger.Error("sql.Open failed", zap.Error(err))
+		os.Exit(1)
 	}
 
 	server := http_server.New(db, logger)
 	router := server.NewRouter()
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	logger.Info("Starting HTTP server on port " + port)
+	port := config.DefaultVars.HTTPPort
+	logger.Info(fmt.Sprintf("Starting HTTP server on port %d", port))
 	if err := http.ListenAndServe(fmt.Sprintf("127.0.0.1:%v", port), router); err != nil {
 		logger.Fatal("http.ListenAndServe failed", zap.Error(err))
 	}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/oinume/todomvc
 go 1.14
 
 require (
+	contrib.go.opencensus.io/exporter/jaeger v0.2.0
 	github.com/friendsofgo/errors v0.9.2
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gofrs/uuid v3.3.0+incompatible // indirect
@@ -15,10 +16,12 @@ require (
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/viper v1.7.0
+	github.com/uber/jaeger-client-go v2.24.0+incompatible // indirect
 	github.com/volatiletech/inflect v0.0.1 // indirect
 	github.com/volatiletech/sqlboiler v3.7.1+incompatible
-	go.opencensus.io v0.22.4 // indirect
+	go.opencensus.io v0.22.4
 	go.uber.org/zap v1.15.0
+	google.golang.org/api v0.28.0 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
 	google.golang.org/protobuf v1.24.0
 )
