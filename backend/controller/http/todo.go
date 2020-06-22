b08d471b8c1a22db37e2edfd815ad63d3e9f35db
Kazuhiro Oinuma <oinume@gmail.com>
2020-06-22 21:40:40 +0900

    Add RPC: UpdateTodo

--- backend/controller/http/todo.go
+++ backend/controller/http/todo.go
@@ -29,3 +29,23 @@ func (s *server) CreateTodo(w http.ResponseWriter, r *http.Request) {
 
 	writeJSON(w, http.StatusCreated, proto.NewTodoConverter().Convert(todo))
 }
+
+func (s *server) UpdateTodo(w http.ResponseWriter, r *http.Request) {
+	req := &todomvc.UpdateTodoRequest{}
+	if err := s.unmarshaler.Unmarshal(r.Body, req); err != nil {
+		internalServerError(s.logger, w, err)
+		return
+	}
+
+	id := uuid.New().String()
+	todo := &model.Todo{
+		ID:    id,
+		Title: req.Title,
+	}
+	if err := s.todoRepo.Create(r.Context(), todo); err != nil {
+		internalServerError(s.logger, w, err)
+		return
+	}
+
+	writeJSON(w, http.StatusCreated, proto.NewTodoConverter().Convert(todo))
+}
