8f41ea0695e9f637ea0e1ef28ee04daa2ec75f58
Kazuhiro Oinuma <oinume@gmail.com>
2020-06-29 06:41:19 +0900

    Only ContextExecutor

--- backend/repository/repository.go
+++ backend/repository/repository.go
@@ -5,17 +5,11 @@ import (
 	"database/sql"
 )
 
-// Executor can perform SQL queries.
-type Executor interface {
+// ContextExecutor can perform SQL queries
+type ContextExecutor interface {
 	Exec(query string, args ...interface{}) (sql.Result, error)
 	Query(query string, args ...interface{}) (*sql.Rows, error)
 	QueryRow(query string, args ...interface{}) *sql.Row
-}
-
-// ContextExecutor can perform SQL queries with context
-type ContextExecutor interface {
-	Executor
-
 	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
 	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
