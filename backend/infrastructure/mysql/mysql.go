71f8284cf1556454f4c0104d4b6f4c50a3446232
Kazuhiro Oinuma <oinume@gmail.com>
2020-07-08 03:48:42 +0900

    Add doc comments

--- backend/infrastructure/mysql/mysql.go
+++ backend/infrastructure/mysql/mysql.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 )
 
+// NewDB connects to database with given `dsn` then returns `*sql.DB` instance.
 func NewDB(dsn string) (*sql.DB, error) {
 	db, err := sql.Open("mysql", dsn)
 	// TODO: db.SetMaxIdleConns, etc...
