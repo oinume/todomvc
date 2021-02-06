5fdf917d4185669b56cb842a1316ab560202d255
Kazuhiro Oinuma <oinume@gmail.com>
2021-02-06 18:13:57 +0900

    Update message

--- backend/modeltest/modeltest_test.go
+++ backend/modeltest/modeltest_test.go
@@ -12,20 +12,20 @@ func TestNewTodo(t *testing.T) {
 	createdAt := time.Date(2021, 1, 1, 0, 1, 30, 0, time.UTC)
 	tests := map[string]struct {
 		setter func(*model.Todo)
-		want *model.Todo
+		want   *model.Todo
 	}{
 		"normal": {
-			setter: func (todo *model.Todo) {
+			setter: func(todo *model.Todo) {
 				todo.ID = "aaa"
 				todo.Title = "aaa"
 				todo.Completed = 1
-				todo.CreatedAt =createdAt
+				todo.CreatedAt = createdAt
 				todo.UpdatedAt = createdAt
 			},
 			want: &model.Todo{
-				ID: "aaa",
+				ID:        "aaa",
 				Completed: 1,
-				Title: "aaa",
+				Title:     "aaa",
 				CreatedAt: createdAt,
 				UpdatedAt: createdAt,
 			},
@@ -34,7 +34,7 @@ func TestNewTodo(t *testing.T) {
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
 			if got := NewTodo(tt.setter); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewTodo() = %v, want %v", got, tt.want)
+				t.Errorf("NewTodo(): got=%v, want=%v", got, tt.want)
 			}
 		})
 	}
